530afb097bca879fce6327b0b2b4af0776483779
git push force <117515380+WhyNeet@users.noreply.github.com>
2025-08-14 22:48:16 +0300

    test(crdt): test multi-actor insertion

--- crates/crdt/tests/rga.rs
+++ crates/crdt/tests/rga.rs
@@ -1,36 +1,15 @@
 use crdt::{Rga, RgaInsertQuery};
 
 #[test]
-pub fn single_actor_rga_left_insertion_works() {
-    let actor_id = 0;
-    let mut rga = Rga::new(actor_id);
-
-    for (idx, c) in "Hello, world!".chars().enumerate() {
-        rga.insert(RgaInsertQuery::Left((actor_id, idx as u64)), c);
-    }
-
-    assert_eq!(rga.to_string(), "Hello, world!");
-}
-
-#[test]
 pub fn single_actor_rga_right_insertion_works() {
     let actor_id = 0;
     let mut rga = Rga::new(actor_id);
 
-    let mut chars = "Hello, world!".chars().enumerate();
-
-    if let Some((idx, c)) = chars.next() {
-        rga.insert(RgaInsertQuery::Left((actor_id, idx as u64)), c);
+    for (idx, c) in "Hello, world!".chars().enumerate() {
+        rga.insert(RgaInsertQuery::Right((actor_id, idx as u64)), c, None);
     }
 
-    for (idx, c) in chars {
-        rga.insert(RgaInsertQuery::Right((actor_id, idx as u64)), c);
-    }
-
-    assert_eq!(
-        rga.to_string().chars().rev().collect::<String>(),
-        "Hello, world!"
-    );
+    assert_eq!(rga.to_string(), "Hello, world!");
 }
 
 #[test]
@@ -39,22 +18,73 @@ pub fn single_actor_rga_middle_insertion_works() {
     let mut rga = Rga::new(actor_id);
 
     let input = "Hello, world!";
-    let mut chars = input.chars().enumerate();
+    let mut chars = input.chars();
 
     let first = chars.next().unwrap();
-    let start_id = rga.insert(RgaInsertQuery::Left((actor_id, 0)), first.1);
+    let start_id = rga.insert(RgaInsertQuery::Right((actor_id, 0)), first, None);
 
     let middle = chars.by_ref().take(input.len() - 2).collect::<Vec<_>>();
 
     let end = chars.next().unwrap();
 
-    let end_id = rga.insert(RgaInsertQuery::Left(start_id), end.1);
+    let end_id = rga.insert(RgaInsertQuery::Right(start_id), end, None);
 
     let mut start_id = start_id;
 
-    for (_, c) in middle {
-        start_id = rga.insert(RgaInsertQuery::Middle(start_id, end_id), c);
+    for c in middle {
+        start_id = rga.insert(RgaInsertQuery::Middle(start_id, end_id), c, None);
     }
 
     assert_eq!(rga.to_string(), "Hello, world!");
+}
+
+#[test]
+pub fn multi_actor_rga_left_insertion_works() {
+    let current_actor_id = 0;
+    let other_actor_id = 1;
+
+    let mut rga = Rga::new(0);
+
+    // Insert "Hello" as current actor
+    let mut prev_id = (current_actor_id, 0);
+    for c in "Hello".chars() {
+        prev_id = rga.insert(RgaInsertQuery::Right(prev_id), c, None);
+    }
+
+    // Current actor wants to put 'a' after letter 'H'
+    // Other actor wants to put 'b' after letter 'H'
+
+    // Case 1:
+    // Edit from current actor comes first, edit from last actor comes second
+    {
+        let mut rga = rga.clone();
+        rga.insert(
+            RgaInsertQuery::Middle((current_actor_id, 1), (current_actor_id, 2)),
+            'a',
+            None,
+        );
+        rga.insert(
+            RgaInsertQuery::Middle((current_actor_id, 1), (current_actor_id, 2)),
+            'b',
+            Some(other_actor_id),
+        );
+        assert_eq!(rga.to_string(), "Habello");
+    }
+
+    // Case 2:
+    // Edit from current actor comes second, edit from last actor comes first
+    {
+        let mut rga = rga.clone();
+        rga.insert(
+            RgaInsertQuery::Middle((current_actor_id, 1), (current_actor_id, 2)),
+            'b',
+            Some(other_actor_id),
+        );
+        rga.insert(
+            RgaInsertQuery::Middle((current_actor_id, 1), (current_actor_id, 2)),
+            'a',
+            None,
+        );
+        assert_eq!(rga.to_string(), "Habello");
+    }
 }
