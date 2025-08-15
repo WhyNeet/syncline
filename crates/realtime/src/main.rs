25dbd118b4c82b240e0f784c9d02d654feaf772d
git push force <117515380+WhyNeet@users.noreply.github.com>
2025-08-15 13:28:44 +0300

    feat(realtime): init http server

--- crates/realtime/src/main.rs
+++ crates/realtime/src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use std::{env, io};
+
+use axum::Router;
+use tokio::net::TcpListener;
+use tracing::log::LevelFilter;
+
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    pretty_env_logger::formatted_builder()
+        .filter_level(LevelFilter::Info)
+        .parse_default_env()
+        .init();
+
+    let router = Router::new();
+
+    let port = env::var("PORT")
+        .ok()
+        .and_then(|s| s.parse().ok())
+        .unwrap_or(8080);
+
+    let listener = TcpListener::bind(("0.0.0.0", port)).await?;
+    tracing::info!("Listener bound on port {port}");
+    axum::serve(listener, router).await
 }
