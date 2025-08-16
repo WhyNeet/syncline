ac2af4c91d614bbf0a5bce6b513b4bd10db61455
git push force <117515380+WhyNeet@users.noreply.github.com>
2025-08-16 12:19:03 +0300

    test(crdt): add deletion test js crdt

--- packages/crdt/spec/index.test.ts
+++ packages/crdt/spec/index.test.ts
@@ -13,3 +13,20 @@ test("Rga insertion works", () => {
 
   expect(rga.toString()).toBe("Hello, world!");
 });
+
+test("Rga deletion works", () => {
+  const rga = new Rga(0, "");
+  const input = "Hello, world!";
+  let prevId = rga.insert([0, 0], input.charAt(0), null, null);
+  expect(prevId).toStrictEqual([0, 1]);
+  for (let i = 1; i < input.length; i++) {
+    prevId = rga.insert(prevId!, input.charAt(i), null, null);
+    expect(prevId).toStrictEqual([0, i + 1]);
+  }
+
+  rga.delete([0, 6]);
+  rga.delete([0, 7]);
+  expect(rga.insert([[0, 7], [0, 8]], " ", null, null)).toStrictEqual([0, 14]);
+
+  expect(rga.toString()).toBe("Hello world!");
+});
