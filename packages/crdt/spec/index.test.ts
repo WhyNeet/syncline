c36b94f2e5ab8c06a8e052c834daab324cf7ca3e
git push force <117515380+WhyNeet@users.noreply.github.com>
2025-08-16 12:24:06 +0300

    test(crdt): add js compaction test

--- packages/crdt/spec/index.test.ts
+++ packages/crdt/spec/index.test.ts
@@ -30,3 +30,22 @@ test("Rga deletion works", () => {
 
   expect(rga.toString()).toBe("Hello world!");
 });
+
+test("Rga compaction works", () => {
+  const rga = new Rga(0, "");
+  const input = "Hello, world!";
+  let prevId = rga.insert([0, 0], input.charAt(0), null, null);
+  expect(prevId).toStrictEqual([0, 1]);
+  for (let i = 1; i < input.length; i++) {
+    prevId = rga.insert(prevId!, input.charAt(i), null, null);
+    expect(prevId).toStrictEqual([0, i + 1]);
+  }
+
+  rga.delete([0, 6]);
+  rga.delete([0, 7]);
+  expect(rga.insert([[0, 7], [0, 8]], " ", null, null)).toStrictEqual([0, 14]);
+
+  rga.compact();
+
+  expect(rga.insert([[0, 6], [0, 7]], " ", null, null)).toBeNull();
+});
